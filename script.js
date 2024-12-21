9bf0fba4f08756427adbdbe08fad00f27670dd8d
Alex Beletskyi <66070246+webrainx@users.noreply.github.com>
2024-12-21 07:31:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 document.getElementById('language-toggle').addEventListener('click', function () {
   const currentFlag = this.getAttribute('src');
-  const currentPage = window.location.pathname; // Get the current page path
+  const basePath = window.location.origin + '/project-name/'; // Replace 'project-name' with your actual GitHub Pages project name
 
-  if (currentFlag === 'images/flags/german_flag.png') {
-    // Switch to German page (index-de.html)
-    this.setAttribute('src', 'images/flags/british_flag.png'); // Change to British flag
+  if (currentFlag.includes('german_flag.png')) {
+    // Switch to German
+    this.setAttribute('src', basePath + 'images/flags/british_flag.png'); 
     this.setAttribute('title', 'Switch to English');
-    window.location.href = currentPage.replace('index.html', 'index-de.html'); // Redirect to the German page
+    window.location.href = basePath + 'index-de.html'; 
   } else {
-    // Switch to English page (index.html)
-    this.setAttribute('src', 'images/flags/german_flag.png'); // Change to German flag
+    // Switch to English
+    this.setAttribute('src', basePath + 'images/flags/german_flag.png'); 
     this.setAttribute('title', 'Switch to German');
-    window.location.href = currentPage.replace('index-de.html', 'index.html'); // Redirect to the English page
+    window.location.href = basePath + 'index.html'; 
   }
 });
+
 window.addEventListener('load', function () {
   const currentPage = window.location.pathname;
   const flagImg = document.getElementById('language-toggle');
+  const basePath = window.location.origin + '/project-name/';
 
   if (currentPage.includes('index-de.html')) {
-    // If we are on the German page, show the English flag
-    flagImg.setAttribute('src', 'images/flags/british_flag.png');
+    flagImg.setAttribute('src', basePath + 'images/flags/british_flag.png');
     flagImg.setAttribute('title', 'Switch to English');
   } else {
-    // If we are on the English page, show the German flag
-    flagImg.setAttribute('src', './images/flags/german_flag.png');
+    flagImg.setAttribute('src', basePath + 'images/flags/german_flag.png');
     flagImg.setAttribute('title', 'Switch to German');
   }
 });
