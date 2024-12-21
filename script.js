40621b8b9f4b4c1a87630e6a3d8ec9bb0575ebaf
Alex Beletskyi <66070246+webrainx@users.noreply.github.com>
2024-12-21 07:40:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 document.getElementById('language-toggle').addEventListener('click', function () {
   const currentFlag = this.getAttribute('src');
-  const basePath = window.location.origin + '/alex-beletskyi-cv/';
+  const basePath = 'https://webrainx.github.io/alex-beletskyi-cv/';
 
   if (currentFlag.includes('german_flag.png')) {
-    // Switch to German
-    this.setAttribute('src', basePath + './images/flags/british_flag.png'); 
+    this.setAttribute('src', basePath + 'images/flags/british_flag.png'); 
     this.setAttribute('title', 'Switch to English');
     window.location.href = basePath + 'index-de.html'; 
   } else {
-    // Switch to English
-    this.setAttribute('src', basePath + './images/flags/german_flag.png'); 
+    this.setAttribute('src', basePath + 'images/flags/german_flag.png'); 
     this.setAttribute('title', 'Switch to German');
     window.location.href = basePath + 'index.html'; 
   }
@@ -18,13 +16,13 @@ document.getElementById('language-toggle').addEventListener('click', function ()
 window.addEventListener('load', function () {
   const currentPage = window.location.pathname;
   const flagImg = document.getElementById('language-toggle');
-  const basePath = window.location.origin + '/alex-beletskyi-cv/';
+  const basePath = 'https://webrainx.github.io/alex-beletskyi-cv/';
 
   if (currentPage.includes('index-de.html')) {
-    flagImg.setAttribute('src', basePath + './images/flags/british_flag.png');
+    flagImg.setAttribute('src', basePath + 'images/flags/british_flag.png');
     flagImg.setAttribute('title', 'Switch to English');
   } else {
-    flagImg.setAttribute('src', basePath + './images/flags/german_flag.png');
+    flagImg.setAttribute('src', basePath + 'images/flags/german_flag.png');
     flagImg.setAttribute('title', 'Switch to German');
   }
 });
