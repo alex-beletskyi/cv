49f98fa4a3192114eb906978ef02c3e346369bc0
Alex Beletskyi <66070246+webrainx@users.noreply.github.com>
2024-12-21 07:38:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.getElementById('language-toggle').addEventListener('click', function ()
 
   if (currentFlag.includes('german_flag.png')) {
     // Switch to German
-    this.setAttribute('src', basePath + 'images/flags/british_flag.png'); 
+    this.setAttribute('src', basePath + './images/flags/british_flag.png'); 
     this.setAttribute('title', 'Switch to English');
     window.location.href = basePath + 'index-de.html'; 
   } else {
     // Switch to English
-    this.setAttribute('src', basePath + 'images/flags/german_flag.png'); 
+    this.setAttribute('src', basePath + './images/flags/german_flag.png'); 
     this.setAttribute('title', 'Switch to German');
     window.location.href = basePath + 'index.html'; 
   }
@@ -21,10 +21,10 @@ window.addEventListener('load', function () {
   const basePath = window.location.origin + '/alex-beletskyi-cv/';
 
   if (currentPage.includes('index-de.html')) {
-    flagImg.setAttribute('src', basePath + 'images/flags/british_flag.png');
+    flagImg.setAttribute('src', basePath + './images/flags/british_flag.png');
     flagImg.setAttribute('title', 'Switch to English');
   } else {
-    flagImg.setAttribute('src', basePath + 'images/flags/german_flag.png');
+    flagImg.setAttribute('src', basePath + './images/flags/german_flag.png');
     flagImg.setAttribute('title', 'Switch to German');
   }
 });
